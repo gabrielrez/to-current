ae70365b7f31c6d3d0e2c2cb456ddb34a9c0f117
gabrielrez <gabrielrezcpessoa@gmail.com>
2024-07-28 21:19:38 -0300

    frontend

---
+++ templates/cute.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cute Email</title>
+  <link rel="stylesheet" href="../style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Roboto:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <div class="container">
+    <header>
+      <h1 class="text-center poppins bold upper">To Current</h1>
+      <span class="detail"></span>
+    </header>
+    <section class="top-section">
+      <h2 class="subtitle">Bom dia!</h2>
+      <p class="paragraph italic">"Então, estou à frente do meu tempo? Tudo bem, me ligue quando você se atualizar."</p>
+    </section>
+    <main>
+      <div class="headline">
+        <span class="poppins bold upper text-yellow">Tipo</span>
+        <div class="sub-headline">
+          <h1 class="roboto semibold">A lua sumiu "do nada"</h1>
+          <img class="img" src="../public/img/lua.webp">
+          <h3 class="subtitle">Introdução</h3>
+          <p class="paragraph">O gato ou gato doméstico é um mamífero carnívoro da família dos felídeos, muito popular como animal de estimação. Ocupando o topo da cadeia alimentar, é predador natural de diversos animais, como roedores, pássaros, lagartixas e alguns insetos.</p>
+        </div>
+        <div class="sub-headline">
+          <h3 class="subtitle">História e domesticação</h3>
+          <p class="paragraph">Os gatos domésticos atuais são uma adaptação evolutiva dos gatos-bravo, com cruzamentos entre diferentes espécimes os tornaram menores e menos agressivos aos humanos.[43] A primeira indicação conhecida à domesticação de um gato-selvagem-africano (F. lybica) foi escavada perto de uma sepultura neolítica humana em Silurocambo, ao sul de Chipre, datando de cerca de 7 500–7 200 a.C.. Uma vez que não há evidências de fauna nativa de mamíferos em Chipre, os habitantes desta aldeia neolítica provavelmente trouxeram o gato e outros mamíferos selvagens à ilha do Oriente Médio continental.</p>
+          <p class="paragraph">O gato ou gato doméstico é um mamífero carnívoro da família dos felídeos, muito popular como animal de estimação.</p>
+        </div>
+      </div>
+      <div class="headline">
+        <span class="poppins bold upper text-blue">Tipo</span>
+        <div class="sub-headline">
+          <h1 class="roboto semibold">Sheldon adota um Gaio Azul</h1>
+          <img class="img" src="../public/img/gaio-azul.jpg">
+          <h3 class="subtitle">Sheldon enlouqueceu?</h3>
+          <p class="paragraph">Sheldon é o melhor amigo e colega de quarto de Leonard Hofstadter Os dois moram juntos na Avenida Los Robles, 2311, Pasadena, CA. Seu nome e o de Leonard foram uma homenagem ao falecido ator/produtor Sheldon Leonard. Os nomes também podem ser referência aos físicos Sheldon Lee Glashow, Leon Neil Cooper e Robert Hofstadter, vencedores do Prêmio Nobel e com importantes contribuições relacionadas com mecânica quântica.</p>
+        </div>
+      </div>
+    </main>
+  </div>
+</body>
+
+</html>
